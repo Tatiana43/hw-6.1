85d4d6b533816cb48cdb0b0d202f78e003ebe914
Tanya <tanya.leukhina@mail.ru>
2020-11-27 00:20:53 +0300

    code edited

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -12,81 +12,66 @@ public class StatsService<monthNumber> {
     }
 
     public long calculateAverageSalesSum(long[] sales) {
-        long sum = 0;
-        for (long sale : sales) {
-            sum = sum + sale;
-        }
+        long sum = calculateSalesSum(sales);
+
         return sum / sales.length;
     }
 
     public long monthWithMaxSales(long[] sales) {
         long currentMax = sales[0];
+        long monthNumber = 0;
+        long monthWithMax = 0;
 
         for (long sale : sales) {
             if (currentMax < sale) {
                 currentMax = sale;
             }
-        }
 
-        long monthNumber = 0;
-        long monthWithMax = 0;
-
-        for (long sale : sales) {
             monthNumber++;
             if (sale == currentMax) {
                 monthWithMax = monthNumber;
             }
+
         }
         return monthWithMax;
     }
 
     public long monthWithMinSales(long[] sales) {
-    long currentMin = sales[0];
+        long currentMin = sales[0];
+        long monthNumber = 0;
+        long monthWithMin = 0;
 
-    for (long sale : sales) {
-        if (currentMin > sale) {
-            currentMin = sale;
+        for (long sale : sales) {
+            if (currentMin > sale) {
+                currentMin = sale;
+            }
+            monthNumber++;
+            if (sale == currentMin) {
+                monthWithMin = monthNumber;
+            }
+
         }
-    }
-
-    long monthNumber = 0;
-    long monthWithMin = 0;
-
-    for (long sale : sales) {
-        monthNumber++;
-        if (sale == currentMin) {
-            monthWithMin = monthNumber;
-        }
-    }
         return monthWithMin;
     }
 
-    public long monthsWithSalesBelowAverage(long[] sales){
+    public long monthsWithSalesBelowAverage(long[] sales) {
         long month = 0;
-        long sum = 0;
+        long averageSum = calculateAverageSalesSum(sales);
 
         for (long sale : sales) {
-            sum = sum + sale;
-        }
-
-        for (long sale : sales) {
-            if (sale < sum / sales.length) {
+            if (sale < averageSum) {
                 month++;
             }
         }
         return month;
     }
 
-    public long monthsWithSalesAboveAverage(long[] sales){
+    public long monthsWithSalesAboveAverage(long[] sales) {
         long month = 0;
-        long sum = 0;
+        long averageSum = calculateAverageSalesSum(sales);
 
         for (long sale : sales) {
-            sum = sum + sale;
-        }
-
-        for (long sale : sales) {
-            if (sale > sum / sales.length) {
+            if (sale > averageSum) {
                 month++;
             }
         }
